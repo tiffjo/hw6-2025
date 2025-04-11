9b0c767b51cab2202cb18b6aa179661d5a6b8123
Sung Eun Jo <jotiff16@gmail.com>
2025-04-11 18:32:07 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,76 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+	video=document.querySelector("#player1");
+	video.autoplay=false;
+	video.loop=false;
+	video.load();
+
+	// set volume
+	document.querySelector("#volume").innerHTML=video.volume*100 + "%";
+	
+	// play video
+	document.querySelector("#play").addEventListener("click", function() {
+		video.play();
+		document.querySelector("#volume").innerHTML=video.volume*100 + "%";
+	});
+
+	// pause video
+	document.querySelector("#pause").addEventListener("click", function() {
+		video.pause();
+	});
+
+	// slow video
+	document.querySelector("#slower").addEventListener("click", function () {
+		video.playbackRate *= 0.9;
+		console.log(`Speed is ${video.playbackRate.toFixed(5)}`);
+	});
+
+	// speed up video
+	document.querySelector("#faster").addEventListener("click", function () {
+		video.playbackRate /= 0.9;
+		console.log(`Speed is ${video.playbackRate.toFixed(5)}`);
+	});
+
+	// Skip Ahead
+	document.querySelector("#skip").addEventListener("click", function () {
+		if (video.currentTime + 10 >= video.duration) {
+			video.currentTime = 0;
+		} else {
+			video.currentTime += 10;
+		}
+		console.log(`Current location: ${video.currentTime.toFixed(2)}s`);
+	});
+
+	// un/mute video
+	document.querySelector("#mute").addEventListener("click", function () {
+		video.muted = !video.muted;
+		this.innerText = video.muted ? "Unmute" : "Mute";
+	});
+
+
+
+	// volume slider
+	document.querySelector("#slider").addEventListener("input", function () {
+		video.volume = this.value / 100;
+		document.querySelector("#volume").innerText = `${this.value}%`;
+	});
+
+	//styled
+
+	document.querySelector("#vintage").addEventListener("click", function () {
+		video.classList.add("oldSchool");
+	});
+
+	// original
+	document.querySelector("#orig").addEventListener("click", function () {
+		video.classList.remove("oldSchool");
+	});
+
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
